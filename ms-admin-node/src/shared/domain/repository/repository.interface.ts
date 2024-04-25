4377169f1dc4250cf2196fe8c461aa38b5e0f5aa
João Macieira <jp.macieira@uol.com.br>
2024-04-25 17:13:31 -0300

    feat: create searchable repository interface

--- ms-admin-node/src/shared/domain/repository/repository.interface.ts
+++ ms-admin-node/src/shared/domain/repository/repository.interface.ts
@@ -1,5 +1,7 @@
 import { Entity } from "../entity";
 import { ValueObject } from "../value-object";
+import { SearchParams } from "./search-params";
+import { SearchResult } from "./search-result";
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   findById(entityId: EntityId): Promise<E | null>;
@@ -10,4 +12,15 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   delete(entityId: EntityId): Promise<void>;
 
   getEntity(): new (...args: any[]) => E;
+}
+
+export interface ISearchebleRepository<
+  E extends Entity,
+  EntityId extends ValueObject,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult,
+> extends IRepository<E, EntityId> {
+  sortabledFields: string[];
+
+  search(props: SearchInput): Promise<SearchOutput>;
 }
