33e0db5a17e69ab7e379df80e41c7491bdc72cce
João Macieira <jp.macieira@uol.com.br>
2024-05-01 18:29:07 -0300

    feat: create category fake builder

---
+++ ms-admin-node/src/category/domain/category-fake.builder.ts
@@ -0,0 +1,121 @@
+import { Chance } from 'chance';
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { Category } from './category.entity';
+
+type PropOrFactory<T> = T | ((index: number) => T);
+
+export class CategoryFakeBuilder<TBuild = any> {
+  private _category_id: PropOrFactory<Uuid> | undefined = undefined;
+  private _name: PropOrFactory<string> = (_index) => this.chance.word();
+  private _description: PropOrFactory<string | null> = (_index) => this.chance.paragraph();
+  private _isActive: PropOrFactory<boolean> = (_index) => true;
+  private _createdAt: PropOrFactory<Date> | undefined = undefined;
+
+  private countObjs;
+
+  static aCategory() {
+    return new CategoryFakeBuilder<Category>();
+  }
+
+  static theCategories(countObjs: number) {
+    return new CategoryFakeBuilder<Category[]>(countObjs);
+  }
+
+  private chance: Chance.Chance;
+
+  private constructor(countObjs: number = 1) {
+    this.countObjs = countObjs;
+    this.chance = Chance();
+  }
+
+  withCategoryId(valueOrFactory: PropOrFactory<Uuid>) {
+    this._category_id = valueOrFactory;
+    return this;
+  }
+
+  withName(valueOrFactory: PropOrFactory<string>) {
+    this._name = valueOrFactory;
+    return this;
+  }
+
+  withDescription(valueOrFactory: PropOrFactory<string | null>) {
+    this._description = valueOrFactory;
+    return this;
+  }
+
+  activate() {
+    this._isActive = true;
+    return this;
+  }
+
+  deactivate() {
+    this._isActive = false;
+    return this;
+  }
+
+  withCreatedAt(valueOrFactory: PropOrFactory<Date>) {
+    this._createdAt = valueOrFactory;
+    return this;
+  }
+
+  withInvalidNameTooLong(value?: string) {
+    this._name = value ?? this.chance.word({ length: 256 });
+    return this;
+  }
+
+  build(): TBuild {
+    const categories = new Array(this.countObjs)
+      .fill(undefined)
+      .map((_, index) => {
+        const category = new Category({
+          categoryId: !this._category_id ? undefined : this.callFactory(this._category_id, index),
+          name: this.callFactory(this._name, index),
+          description: this.callFactory(this._description, index),
+          isActive: this.callFactory(this._isActive, index),
+          ...(this._createdAt && {
+            createdAt: this.callFactory(this._createdAt, index),
+          }),
+        })
+        return category;
+      });
+    return this.countObjs === 1 ? (categories[0] as any) : categories;
+  }
+
+  get categoryId() {
+    return this.getValue('category_id');
+  }
+
+  get name() {
+    return this.getValue('name');
+  }
+
+  get description() {
+    return this.getValue('description');
+  }
+
+  get isActive() {
+    return this.getValue('isActive');
+  }
+
+  get createdAt() {
+    return this.getValue('createdAt');
+  }
+
+  private getValue(prop: any) {
+    const optional = ['category_id', 'createdAt'];
+    const privateProp = `_${prop}` as keyof this;
+    console.log({ prop })
+    if (!this[privateProp] && optional.includes(prop)) {
+      throw new Error(
+        `Property ${prop} not have a factory, use 'with' methods`,
+      );
+    }
+    return this.callFactory(this[privateProp], 0)
+  }
+
+  private callFactory(factoryOrValue: PropOrFactory<any>, index: number) {
+    return typeof factoryOrValue === 'function'
+      ? factoryOrValue(index)
+      : factoryOrValue;
+  }
+}
