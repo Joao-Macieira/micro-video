78a10a291df232ad791b50106576362ba4fe2feb
João Macieira <jp.macieira@uol.com.br>
2024-06-07 17:52:03 -0300

    feat: get category use case

---
+++ ms-admin-node/src/category/application/get-category.use-case.ts
@@ -0,0 +1,39 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { NotFoundError } from "../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../domain/category.entity";
+import { ICategoryRepository } from "../domain/category.repository";
+
+type GetCategoryInput = {
+  id: string;
+};
+
+type GetCategoryOutput = {
+  id: string;
+  name: string;
+  description: string;
+  isActive: boolean;
+  createdAt: Date;
+};
+
+export class GetCategoryUseCase implements IUseCase<GetCategoryInput, GetCategoryOutput> {
+  constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+  async execute(input: GetCategoryInput): Promise<GetCategoryOutput> {
+    const uuid = new Uuid(input.id);
+
+    const category = await this.categoryRepository.findById(uuid);
+
+    if (!category) {
+      throw new NotFoundError(input.id, Category);
+    }
+
+    return {
+      id: category.categoryId.id,
+      name: category.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    }
+  }
+}
