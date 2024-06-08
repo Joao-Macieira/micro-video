e00e539b146d24d22df9d601abd125944b91c134
João Macieira <jp.macieira@uol.com.br>
2024-06-08 19:33:11 -0300

    feat: add coverage configs and ts check configs

--- ms-admin-node/jest.config.ts
+++ ms-admin-node/jest.config.ts
@@ -10,7 +10,22 @@ const config: Config = {
   },
   setupFilesAfterEnv: [
     './shared/infra/testing/expect-helpers.ts'
-  ]
+  ],
+  coverageDirectory: '../.coverage',
+  coverageReporters: [
+    'json',
+    'text',
+    'lcov',
+    'clover',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
 
 export default config;
